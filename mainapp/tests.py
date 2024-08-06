f9d2ad7bf438c8d0d0ccbc7f2f0fa279d1195b15
PavukPepe <sergeupilkewech@gmail.com>
2024-08-06 14:55:56 +0300

    fix6

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -8,3 +8,7 @@ class SimpleTest(TestCase):
     def test_second_object_in_db(self):
         obj2 = models.SimpleModel.objects.get(pk=2)
         self.assertEqual(obj2.just_text, 'text_2')
+
+    def test_3_object_in_db(self):
+        obj2 = models.SimpleModel.objects.get(pk=3)
+        self.assertEqual(obj2.just_text, 'text_3')
