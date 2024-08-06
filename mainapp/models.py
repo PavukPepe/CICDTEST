3c9f1e96397c5fbe6846b83c650f5844fa67b1cc
PavukPepe <sergeupilkewech@gmail.com>
2024-08-06 13:19:58 +0300

    first

---
+++ mainapp/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+
+class SimpleModel(models.Model):
+    just_text = models.CharField(verbose_name='Just a field', max_length=200)
---
+++ mainapp/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from . import models
+
+class SimpleTest(TestCase):
+    def test_first_object_in_db(self):
+        obj1 = models.SimpleModel.objects.first()
+        self.assertEqual(obj1.just_text, 'text_1')
+    def test_second_object_in_db(self):
+        obj2 = models.SimpleModel.objects.get(pk=2)
+        self.assertEqual(obj2.just_text, 'text_2')
